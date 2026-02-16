bf56aacc3e026fb77e7f6de87c56a95c20962a93
Gravitazione <sasipas.chi@gmail.com>
2026-02-16 22:12:16 +0700

    chore: delete the deprecated command, config app module with multi env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,15 @@ import { UserModule } from '@/user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { HealthModule } from '@/health/health.module';
 
+const nodeEnv = process.env.NODE_ENV ?? 'development';
+
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', `.env.${nodeEnv}`],
+      ignoreEnvFile: false,
+    }),
     PrismaModule,
     UserModule,
     HealthModule,
