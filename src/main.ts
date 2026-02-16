a28b76a1ce23371786b3e44b1205024535246bb6
Gravitazione <sasipas.chi@gmail.com>
2026-02-16 21:03:44 +0700

    feat: Add environment configuration, enhance security with helmet, and implement global validation in main application. Introduce Prisma module for database interactions and update dependencies in package.json and pnpm-lock.yaml.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.APP_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  app.use(helmet());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
